e96d61c2cd3a9efe92c9f574dd1add738a2bc9f2
edwin <edwinslottje@gmail.com>
2021-11-29 15:23:28 +0100

    Small changes + Oxygen resource updates

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,6 +9,11 @@ public class HealthBar : MonoBehaviour
     public Image healthBar;
     public float health;
     public float maxHealth;
+
+    //Damage over time
+    float timer = 0;
+    public bool takenDamage;
+    public float maxTime = 2;
     void Start()
     {
         health = 100;
@@ -22,6 +27,29 @@ public class HealthBar : MonoBehaviour
         if (health <= 0)
         {
             SceneManager.LoadScene("Level layout");
+        }
+        TakeDamageOverTime(1);
+    }
+
+    public void TakeDamageOverTime(int damage)
+    {
+        if (health > 0)
+        {
+            if (!takenDamage)
+            {
+                timer += Time.deltaTime;    
+                if (timer > maxTime)
+                {
+                    takenDamage = true;
+                }
+            }
+            if (takenDamage)
+            {
+                health -= damage;
+                timer = 0;
+                takenDamage = false;
+            }
+
         }
     }
 
